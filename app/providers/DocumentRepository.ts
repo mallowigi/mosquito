3d533c1376284a673fb37132086649314199e393
Elior <elior.b@dynamicyield.com>
2016-01-16 17:09:37 +0200

    search

--- app/providers/DocumentRepository.ts
+++ app/providers/DocumentRepository.ts
@@ -9,6 +9,7 @@ import {Filters} from "./Filters";
 import {UserData} from "./UserData";
 
 import * as _ from "lodash";
+import * as R from "ramda";
 import {IComment} from "../types/IComment";
 import {IUser} from "../types/IUser";
 
@@ -17,15 +18,15 @@ var faker = require('faker');
 
 @Injectable()
 export class DocumentRepository {
-    private http:Http;
-    private userData:UserData;
+    private http: Http;
+    private userData: UserData;
 
-    constructor(http:Http, userData:UserData) {
+    constructor(http: Http, userData: UserData) {
         this.http = http;
         this.userData = userData;
     }
 
-    getLatest():Promise<IDocument[]> {
+    getLatest(): Promise<IDocument[]> {
         return new Promise(resolve => {
             //this.http.get('data/dashboard.json')
             //    .subscribe(res => {
@@ -35,14 +36,24 @@ export class DocumentRepository {
         });
     }
 
-    filterList(documents:IDocument[], queryText:string, filter:Filters):IDocument[] {
+    filterList(documents: IDocument[], queryText: string, filter: Filters): IDocument[] {
+        let query = new RegExp(queryText, 'i');
+        let fDocs = R.filter(R.or(
+            R.where({
+                title: R.test(query)
+            }),
+            R.where({
+                description: R.test(query)
+            })
+        ))(documents);
+
         switch (filter) {
             case Filters.Friends:
-                return _.filter(documents, (doc) => {
+                return _.filter(fDocs, (doc) => {
                     return _.contains(this.userData.friends, doc.author)
                 });
             case Filters.All:
-                return documents;
+                return fDocs;
         }
 
     }
@@ -50,15 +61,15 @@ export class DocumentRepository {
     private static generateDashboard() {
         return _.times(10, () => {
             return {
-                title: faker.commerce.productName(),
+                title      : faker.commerce.productName(),
                 description: faker.lorem.sentence(),
-                id: faker.random.number(),
-                author: DocumentRepository.generateAuthor(),
-                created: faker.date.recent(),
-                updated: faker.date.recent(),
-                imageUrl: faker.image.imageUrl(),
-                likes: faker.random.number({max: 100}),
-                comments: DocumentRepository.generateComments()
+                id         : faker.random.number(),
+                author     : DocumentRepository.generateAuthor(),
+                created    : faker.date.recent(),
+                updated    : faker.date.recent(),
+                imageUrl   : faker.image.imageUrl(),
+                likes      : faker.random.number({max: 100}),
+                comments   : DocumentRepository.generateComments()
             }
         })
     }
@@ -66,33 +77,33 @@ export class DocumentRepository {
     private generateDocuments() {
         return _.times(10, () => {
             return {
-                title: faker.commerce.productName(),
+                title      : faker.commerce.productName(),
                 description: faker.lorem.sentence(),
-                id: faker.random.number(),
-                author: this.userData,
-                created: faker.date.recent(),
-                updated: faker.date.recent(),
-                imageUrl: faker.image.imageUrl(),
-                likes: faker.random.number({max: 100}),
-                comments: DocumentRepository.generateComments()
+                id         : faker.random.number(),
+                author     : this.userData,
+                created    : faker.date.recent(),
+                updated    : faker.date.recent(),
+                imageUrl   : faker.image.imageUrl(),
+                likes      : faker.random.number({max: 100}),
+                comments   : DocumentRepository.generateComments()
             }
         })
     }
 
-    private static generateComments():IComment[] {
+    private static generateComments(): IComment[] {
         return _.times(faker.random.number({max: 30}), () => {
             return {
                 message: faker.company.catchPhrase(),
-                author: DocumentRepository.generateAuthor(),
+                author : DocumentRepository.generateAuthor(),
                 created: faker.date.recent()
             };
         });
     }
 
-    private static generateAuthor():IUser {
+    private static generateAuthor(): IUser {
         return {
-            name: faker.internet.userName(),
-            id: faker.random.number(),
+            name  : faker.internet.userName(),
+            id    : faker.random.number(),
             avatar: faker.image.avatar()
         };
     }
